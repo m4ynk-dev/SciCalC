e4896b4ca05bcf10b87c49d4e01a1a3f2ea8d4b3
r17e8h <riteshkr.rana18@gmail.com>
2026-04-15 22:21:55 +0530

    fix: transfer main.c code into a header file trig_functions.hfor clean codebase and overwrite prevention

--- trig_functions.c
+++ trig_functions.c
@@ -1,87 +1,86 @@
 #define _USE_MATH_DEFINES
+#include "trig_functions.h"
 #include <math.h>
 
 // helper functions
-double to_radians(double degrees) {
-    return degrees * (M_PI / 180.0);
-}
+double to_radians(double degrees) { return degrees * (M_PI / 180.0); }
 
-double to_degrees(double radians) {
-    return radians * (180.0 / M_PI);
-}
+double to_degrees(double radians) { return radians * (180.0 / M_PI); }
 
 // standard trigo functions
 double calc_sin(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    return sin(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  return sin(angle);
 }
 
 double calc_cos(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    return cos(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  return cos(angle);
 }
 
 double calc_tan(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    return tan(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  return tan(angle);
 }
-
-
 
 // reciprocal trig functions
 double calc_csc(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    // Cosec is 1 / sin
-    return 1.0 / sin(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  // Cosec is 1 / sin
+  return 1.0 / sin(angle);
 }
 
 double calc_sec(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    // Sec is 1 / cos
-    return 1.0 / cos(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  // Sec is 1 / cos
+  return 1.0 / cos(angle);
 }
 
 double calc_cot(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    // Cot is 1 / tan
-    return 1.0 / tan(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  // Cot is 1 / tan
+  return 1.0 / tan(angle);
 }
-
-
 
 // inverse trigo functions
 
 double calc_asin(double value, int want_degrees) {
-    double result = asin(value);
-    return want_degrees ? to_degrees(result) : result;
+  double result = asin(value);
+  return want_degrees ? to_degrees(result) : result;
 }
 
 double calc_acos(double value, int want_degrees) {
-    double result = acos(value);
-    return want_degrees ? to_degrees(result) : result;
+  double result = acos(value);
+  return want_degrees ? to_degrees(result) : result;
 }
 
 double calc_atan(double value, int want_degrees) {
-    double result = atan(value);
-    return want_degrees ? to_degrees(result) : result;
+  double result = atan(value);
+  return want_degrees ? to_degrees(result) : result;
 }
 
-
-
-
-// hyperbolic trigo functions 
+// hyperbolic trigo functions
 
 double calc_sinh(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    return sinh(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  return sinh(angle);
 }
 
 double calc_cosh(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    return cosh(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  return cosh(angle);
 }
 
 double calc_tanh(double angle, int is_degrees) {
-    if (is_degrees) angle = to_radians(angle);
-    return tanh(angle);
+  if (is_degrees)
+    angle = to_radians(angle);
+  return tanh(angle);
 }
